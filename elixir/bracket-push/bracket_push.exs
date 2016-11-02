2fa412c934d56fe71c654ce6fc92a12df1acf9c6
Manassarn Manoonchai <manassarn@gmail.com>
2016-11-02 22:09:04 +0700

    Elixir : Bracket push (Inside-out)

--- elixir/bracket-push/bracket_push.exs
+++ elixir/bracket-push/bracket_push.exs
@@ -1,27 +1,18 @@
 defmodule BracketPush do
-  @pairs %{
-    "{" => "}",
-    "[" => "]",
-    "(" => ")"
-  }
-
   @doc """
   Checks that all the brackets and braces in the string are matched correctly, and nested correctly
   """
   @spec check_brackets(String.t) :: boolean
   def check_brackets(str) do
-    do_check_brackets(Regex.replace(~r/[^\{\}\(\)\[\]]/, str, ""))
+    check_only_brackets(Regex.replace(~r/[^\{\}\(\)\[\]]/, str, ""))
   end
 
-  defp do_check_brackets(""), do: true
-  defp do_check_brackets(str) do
-    close = @pairs[String.first(str)]
+  defp check_only_brackets(""), do: true
+  defp check_only_brackets(str) do
+    next = Regex.replace(~r/(\{\}|\(\)|\[\])/, str, "")
     cond do
-      String.at(str, 1) === close ->
-        do_check_brackets(String.slice(str, 2..-1))
-      String.at(str, -1) === close ->
-        do_check_brackets(String.slice(str, 1..-2))
-      true -> false
+      next === str -> false
+      true -> check_only_brackets(next)
     end
   end
 end
