19c7bd9326a6fa9fdd54f0bd882e41c486abd043
Manassarn Manoonchai <manassarn@gmail.com>
2016-12-24 14:18:41 +0700

    Elixir : Pangram

---
+++ elixir/pangram/pangram.exs
@@ -0,0 +1,24 @@
+defmodule Pangram do
+  @doc """
+  Determines if a word or sentence is a pangram.
+  A pangram is a sentence using every letter of the alphabet at least once.
+
+  Returns a boolean.
+
+    ## Examples
+
+      iex> Pangram.pangram?("the quick brown fox jumps over the lazy dog")
+      true
+
+  """
+
+  @spec pangram?(String.t) :: boolean
+  def pangram?(""), do: false
+  def pangram?(sentence) do
+    26 == Regex.replace(~r/[^a-zA-Z]/, sentence, "")
+    |> String.downcase
+    |> String.codepoints
+    |> Enum.uniq
+    |> length
+  end
+end
