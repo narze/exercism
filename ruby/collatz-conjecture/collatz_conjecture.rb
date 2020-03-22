e9db2dfcbc963a7d7138f960bf43c54a30b270e1
Manassarn Manoonchai <manassarn@gmail.com>
2020-03-22 19:04:12 +0700

    :recycle: Ruby: Use recursive style

--- ruby/collatz-conjecture/collatz_conjecture.rb
+++ ruby/collatz-conjecture/collatz_conjecture.rb
@@ -1,19 +1,14 @@
 class CollatzConjecture
   def self.steps(num)
-    raise ArgumentError if num < 1
-
-    count = 0
-
-    while num > 1
-      count += 1
-
-      if num.even?
-        num /= 2
-      else
-        num = 3 * num + 1
-      end
+    case
+    when num < 1
+      raise ArgumentError
+    when num == 1
+      0
+    when num.even?
+      1 + steps(num / 2)
+    else
+      1 + steps(3 * num + 1)
     end
-
-    count
   end
 end
