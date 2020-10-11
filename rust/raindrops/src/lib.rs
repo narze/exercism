a87969db2b9726c3a2bc840fa88843074405436d
Manassarn Manoonchai <manassarn@gmail.com>
2020-10-11 23:53:55 +0700

    Rust: Raindrops

---
+++ rust/raindrops/src/lib.rs
@@ -0,0 +1,21 @@
+pub fn raindrops(n: u32) -> String {
+    let mut out = String::from("");
+
+    if n % 3 == 0 {
+      out.push_str("Pling")
+    }
+
+    if n % 5 == 0 {
+      out.push_str("Plang")
+    }
+
+    if n % 7 == 0 {
+      out.push_str("Plong")
+    }
+
+    if out.len() > 0 {
+      out
+    } else {
+      format!("{}", n)
+    }
+}
