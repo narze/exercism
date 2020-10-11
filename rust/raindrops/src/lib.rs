0d20e4ba8a3bc10779dc52a8a2c0a792767ccff0
Manassarn Manoonchai <manassarn@gmail.com>
2020-10-11 23:57:06 +0700

    chore: reformat code

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -2,20 +2,20 @@ pub fn raindrops(n: u32) -> String {
     let mut out = String::from("");
 
     if n % 3 == 0 {
-      out.push_str("Pling")
+        out.push_str("Pling")
     }
 
     if n % 5 == 0 {
-      out.push_str("Plang")
+        out.push_str("Plang")
     }
 
     if n % 7 == 0 {
-      out.push_str("Plong")
+        out.push_str("Plong")
     }
 
     if out.len() > 0 {
-      out
+        out
     } else {
-      format!("{}", n)
+        format!("{}", n)
     }
 }
--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,6 +1,6 @@
 pub fn reverse(input: &str) -> String {
     let mut output = "".to_string();
-    
+
     for i in input.chars().rev() {
         output.push(i);
     }
