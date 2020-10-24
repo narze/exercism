0702185ddd23fe8776cda8606efeee4209200750
Manassarn Manoonchai <manassarn@gmail.com>
2020-10-24 19:39:04 +0700

    Use sum

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -1,9 +1,9 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    (1..=n).fold(0, |acc, x| acc + x).pow(2)
+    (1..=n).sum::<u32>().pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    (1..=n).fold(0, |acc, x| acc + x.pow(2))
+    (1..=n).map(|x| x.pow(2)).sum()
 }
 
 pub fn difference(n: u32) -> u32 {
