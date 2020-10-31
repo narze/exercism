024297bc403d19eed6de8fd6dadae1db14cb6408
Manassarn Manoonchai <manassarn@gmail.com>
2020-10-31 14:40:18 +0700

    refactor: Rust: Armstrong Numbers

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,13 +1,13 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    let digits = to_digits(num);
-    let power: u32 = num.to_string().len() as u32;
+    let num_str = num.to_string();
+    let power: u32 = num_str.len() as u32;
+    let digits = to_digits(num_str);
 
-    num == digits.iter().map(|d| d.pow(power)).sum::<u32>()
+    num == digits.iter().map(|d| d.pow(power)).sum()
 }
 
-fn to_digits(num: u32) -> Vec<u32> {
-    num.to_string()
-        .chars()
+fn to_digits(num_str: String) -> Vec<u32> {
+    num_str.chars()
         .map(|c| c.to_digit(10).unwrap())
         .collect::<Vec<u32>>()
 }
